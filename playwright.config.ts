cdbf378adae9445501e1d1d910ef544e89370b63
harsh.bhagat <harsh.bhagat@clanap.com>
2026-07-22 15:00:59 +0530

    Updated

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: 'http://127.0.0.1:62751',
+    baseURL: process.env.BASE_URL || 'http://127.0.0.1:54331',
     headless: true,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
